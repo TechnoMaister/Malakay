a54481d971ffdc487a802ba0f6bb9dd22a3ab571
bl1nk15 <128423127+bl1nk15@users.noreply.github.com>
2025-01-31 21:09:30 +0200

    home

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -10,8 +10,8 @@ public class LConstants {
         TwoWheelConstants.strafeTicksToInches = .0007;
         TwoWheelConstants.forwardY = -4.1338582677;
         TwoWheelConstants.strafeX = -7.874015748;
-        TwoWheelConstants.forwardEncoder_HardwareMapName = "forwardEncoder";
-        TwoWheelConstants.strafeEncoder_HardwareMapName = "strafeEncoder";
+        TwoWheelConstants.forwardEncoder_HardwareMapName = "leftFront";
+        TwoWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
         TwoWheelConstants.forwardEncoderDirection = Encoder.REVERSE;
         TwoWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
         TwoWheelConstants.IMU_HardwareMapName = "imu";
